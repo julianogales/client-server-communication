53499e51c3bf742cfc4bac08e6f1a39092ae5663
julianogales <julianolo71@gmail.com>
2024-04-17 18:46:44 +0200

    feat: read config and controllers files

--- server.c
+++ server.c
@@ -1,32 +1,62 @@
-// Read file 'client.cfg' and save the information (Name, Situation, Elements, MAC, Local, Server, Srv-UDP) inside
-// config_data
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*
-def read_config():
-    file = open('client.cfg', 'r')
-    try:
-        for line in file:
-            key, value = line.strip().split(' = ')
+/* ------------------------------------------------ GLOBAL VARIABLES ------------------------------------------------ */
 
-            config_data[key] = value
-    finally:
-        file.close()
-*/
+/* STRUCTURES */
+
+/* Store information from server.cfg (previously read in read_config()) into config_data structure */
+typedef struct {
+    char Name[10];
+    char MAC[20];
+    int UDP_port;
+    int TCP_port;
+} ConfigData;
+ConfigData config_data;
+
+/* Store information from server.cfg (previously read in read_config()) into config_data structure */
+typedef struct {
+    char Name[10];
+    char MAC[20];
+    char State[15];
+} ControllersData;
+ControllersData controllers_data;
+
+/* ----------------------------------------------------------------------------------------------------------------- */
 
 
-// Read file 'server.cfg' and save the information (Name, MAC, UDP-port, TCP-port) inside config_data.
+
+/* Read file 'server.cfg' and save the information (Name, MAC, UDP-port, TCP-port) inside config_data.*/
 void read_config() {
     FILE *file = fopen("server.cfg", "r");
     char line[25];
-    char config_data;
 
     while (fgets(line, sizeof(line), file)) {
         char *key = strtok(line, " =");
         char *val = strtok(NULL, " =\n");
-        printf("%s : %s\n", key, val);
+
+        if (strcmp(key, "Name") == 0) { strcpy(config_data.Name, val);}
+        else if (strcmp(key, "MAC") == 0) {strcpy(config_data.MAC, val);}
+        else if (strcmp(key, "UDP-port") == 0) {config_data.UDP_port = atoi(val);}
+        else if (strcmp(key, "TCP-port") == 0) {config_data.TCP_port = atoi(val);}
+    }
+
+    fclose(file);
+}
+
+/* Read file 'server.cfg' and save the information (Name, MAC, UDP-port, TCP-port) inside config_data.*/
+void read_controllers() {
+    FILE *file = fopen("controllers.dat", "r");
+    char line[25];
+
+    while (fgets(line, sizeof(line), file)) {
+        char *key = strtok(line, ",");
+        char *val = strtok(NULL, "\n");
+
+        if (strcmp(key, "Name") == 0) { strcpy(config_data.Name, val);}
+        else if (strcmp(key, "MAC") == 0) {strcpy(config_data.MAC, val);}
+        else if (strcmp(key, "State") == 0) {config_data.UDP_port = atoi(val);}
     }
 
     fclose(file);
@@ -34,4 +64,5 @@ void read_config() {
 
 int main() {
     read_config();
+    return 0;
 }
